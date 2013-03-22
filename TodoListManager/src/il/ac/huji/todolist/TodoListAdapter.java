6829bcd0581f3593f4c9af7ef75bf9a78d440cb0
ocastel <or.castel@gmail.com>
2013-03-22 02:44:06 +0200

    java files

--- TodoListManager/src/il/ac/huji/todolist/TodoListAdapter.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListAdapter.java
@@ -1,6 +1,10 @@
 package il.ac.huji.todolist;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import android.graphics.Color;
 import android.view.LayoutInflater;
@@ -10,25 +14,43 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 
-public class TodoListAdapter extends ArrayAdapter<String> {
+public class TodoListAdapter extends ArrayAdapter<ToDoTask> {
 
-	public TodoListAdapter(TodoListManagerActivity activity, ArrayList<String> todoList) {
+	public TodoListAdapter(TodoListManagerActivity activity, ArrayList<ToDoTask> todoList) {
 			super(activity, android.R.layout.simple_list_item_1, todoList);
 		}
 
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		String todo = getItem(position);
 		LayoutInflater inflater = LayoutInflater.from(getContext());
 		View view = inflater.inflate(R.layout.row, null);
-		TextView rowText = (TextView)view.findViewById(R.id.rowText);
-		rowText.setText(todo);
-		if (position%2 == 0) {
-			rowText.setTextColor(Color.RED);
+		TextView txtTitle = (TextView)view.findViewById(R.id.txtTodoTitle);
+		TextView txtDate = (TextView)view.findViewById(R.id.txtTodoDueDate);
+
+		ToDoTask task = getItem(position);
+		txtTitle.setText(task._task);
+		if (task._date == "No due date")
+		{
+			txtDate.setText("No due date");
 		}
-		else {
-			rowText.setTextColor(Color.BLUE); 
+		else 
+		{
+			txtDate.setText(task._date);		
+	
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+			Date task_date = null;
+			try {
+				task_date = sdf.parse(task._date);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			Date now = new Date();
+			if (now.after(task_date))
+			{
+				txtTitle.setTextColor(Color.RED);
+				txtDate.setTextColor(Color.RED);
+			}
 		}
 		return view;
 	}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,30 +1,68 @@
 package il.ac.huji.todolist;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
+import android.content.Intent;
 
 public class TodoListManagerActivity extends Activity {
 
     private TodoListAdapter adapter;
-    private ArrayList<String> todoList;
+    private ArrayList<ToDoTask> todoList;
     private ListView todoListView;
     
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
-        todoList = new ArrayList<String>();
+        todoList = new ArrayList<ToDoTask>();
         todoListView = (ListView)findViewById(R.id.lstTodoItems);
         adapter = new TodoListAdapter(this, todoList);
-        todoListView.setAdapter(adapter);        
+        todoListView.setAdapter(adapter);
+        registerForContextMenu(todoListView);
     }
     
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		getMenuInflater().inflate(R.menu.ctxmenu, menu);		
+		
+		AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		ToDoTask task = (ToDoTask) todoListView.getItemAtPosition(info.position);
+		menu.setHeaderTitle(task._task);
+		if (!task._task.startsWith("Call "))
+			{
+		 	menu.removeItem(R.id.menuItemCall);
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		int selectedItemIndex = info.position;
+		switch (item.getItemId()){
+		case R.id.menuItemDelete:
+			adapter.remove(adapter.getItem(selectedItemIndex));
+			break;
+		case R.id.menuItemCall:
+			ToDoTask task = adapter.getItem(selectedItemIndex);
+			Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+task._task.substring(5))); 
+			startActivity(dial); 
+		}
+		return true;
+	}
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	getMenuInflater().inflate(R.menu.main, menu);
@@ -34,15 +72,31 @@ public class TodoListManagerActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
-    	case R.id.menuItemAdd:
-    		EditText newToDo = (EditText) findViewById(R.id.edtNewItem);
-    		adapter.add(newToDo.getText().toString());
+	    	case R.id.menuItemAdd:
+    		Intent intent = new Intent(this, AddNewTodoItemActivity.class); 
+    		startActivityForResult(intent, 42); 
     		break;
-    	case R.id.menuItemDelete:
-    		todoList.remove(todoListView.getSelectedItemPosition());
-    		adapter.notifyDataSetChanged();
     	}
-    	return true;
-    }    	
+	    return true;
+    }
+    	
+    protected void onActivityResult(int reqCode, int resCode, Intent data) { 
+    	if (resCode == RESULT_CANCELED)
+    	{
+    		return;
+    	}
+    	switch (reqCode) { 
+    	case 42:
+    		Date date = null;
+    		if (data.getExtras().containsKey("dueDate") )
+    		{
+        		date = (Date) data.getSerializableExtra("dueDate");    			
+    		}
+    		String task = data.getStringExtra("task");
+     		adapter.add(new ToDoTask(task, date));
+     	break;
+    	}
+    }
+
 }
 	
